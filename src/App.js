76647488d3a37102d04cd4706acf6a81e4c13901
codesinc-repo <codes.inc.com@gmail.com>
2024-02-03 18:13:23 +0500

    code updated App.js |pageinsight

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import Routes from "./Routes"
-import ReactModal from 'react-modal';
+import Routes from "./Routes";
+import ReactModal from "react-modal";
 // import Home from "./Pages/Home";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-ReactModal.setAppElement('#root');
+ReactModal.setAppElement("#root");
 function App() {
   return (
     // <Home/>
-   <Routes/>
+    <>
+      <Routes />
+      <SpeedInsights />
+    </>
   );
 }
 
