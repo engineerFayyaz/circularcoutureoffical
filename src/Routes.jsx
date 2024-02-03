1e7421c1f29fbb66f31e6e52a785d11a82e38b8a
codesinc-repo <codes.inc.com@gmail.com>
2024-02-03 11:59:26 +0500

    code updated|Routes |Fayyaz

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -63,8 +63,8 @@ const ProjectRoutes =()=>{
     <React.Suspense fallback={<>Loading...</>}>
         <Router>
         <Routes>
-        <Route path="" element={<Home />} />  
-        <Route path="/Home" element={<Home />} />  
+        {/* <Route path="" element={<Home />} />   */}
+        <Route path="/" element={<Home />} />  
         <Route path="/Collections/NewArrival" element={<NewArrival/>}/>     
         <Route path="/Collections/Accessories" element={<Accessories/>} />       
         <Route path="/Collections/Clothing" element={<CLothing/>} /> 
