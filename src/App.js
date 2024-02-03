bb7b4f48dcca85b617b75e3bcf7067551fd7de6a
codesinc-repo <codes.inc.com@gmail.com>
2024-02-03 18:18:14 +0500

    remove of speed insight

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import React from "react";
 import Routes from "./Routes";
 import ReactModal from "react-modal";
 // import Home from "./Pages/Home";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-
 ReactModal.setAppElement("#root");
 function App() {
   return (
     // <Home/>
     <>
       <Routes />
-      <SpeedInsights />
     </>
   );
 }
